27efe790ef1f209df3f407b744571f7debe0d05c
AndressaSilva0 <andressasp68@gmail.com>
2025-09-15 16:14:30 -0300

    Resolve merge by keeping new User model and add Company, Course, Module models

---
+++ damas-tech-app/app/Models/Module.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Module extends Model
+{
+    protected $fillable = ['course_id', 'title', 'content', 'order_number'];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+
+    public function userProgress()
+    {
+        return $this->hasMany(UserModuleProgress::class, 'module_id');
+    }
+}
---
+++ damas-tech-app/app/Models/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use App\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable, HasRoles;
+
+    protected $fillable = ['name', 'email', 'password', 'role'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    public function company()
+    {
+        return $this->hasOne(Company::class, 'users_id');
+    }
+
+    public function courseProgress()
+    {
+        return $this->hasMany(UserCourseProgress::class, 'users_id');
+    }
+
+    public function moduleProgress()
+    {
+        return $this->hasMany(UserModuleProgress::class, 'users_id');
+    }
+
+    public function talentPool()
+    {
+        return $this->hasMany(TalentPool::class, 'users_id');
+    }
+
+    public function authTokens()
+    {
+        return $this->hasMany(AuthToken::class, 'users_id');
+    }
+
+    public function loginAttempts()
+    {
+        return $this->hasMany(LoginAttempt::class, 'users_id');
+    }
+
+    public function passwordResets()
+    {
+        return $this->hasMany(PasswordReset::class, 'users_id');
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class, 'users_id');
+    }
+
+    public function externalAuth()
+    {
+        return $this->hasMany(ExternalAuth::class, 'users_id');
+    }
+}
